07a09e84280c9844ba93709bf7d582adc2d022de
Divyansh <pateldivyansh131@gmail.com>
2025-10-30 15:04:49 +0530

    update

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 
-function RestaurantCard() {
+function RestaurantCard({restaurant}) {
+  console.log(restaurant , "rest");
   return (
-    <div className='m-2'>
-        <img className='w-58 h-42 rounded-2xl' src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/9/1/9afabb63-f704-4437-acbf-c7a6b7a72d11_101574.JPG" />
+    <div className='m-2 w-58'>
+        <img className='w-58 h-42 rounded-2xl' src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${restaurant.info.cloudinaryImageId}`} />
         <div className='ml-4'>
-            <h1 className='font-bold text-xl'>PIZZA HUT</h1>
-            <span className='font-bold'>4.2</span>
-            <span className='font-bold mx-4'>25-30 Mins</span>
-            <p>Pizza snacks</p>
-            <p>Civil lines</p>
+            <h1 className='font-bold text-xl'>{restaurant.info.name}</h1>
+            <span className='font-bold'>{restaurant.info.avgRating}</span>
+            <span className='font-bold mx-4'>{restaurant.info.sla.slaString}</span>
+            <p>{restaurant.info.cuisines[0]}</p>
+            <p>{restaurant.info.locality}</p>
         </div>
     </div>
   )
